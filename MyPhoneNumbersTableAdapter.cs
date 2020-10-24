fe9cbc715add1cf22258dbdfc1fa3b7914fefee8
george oanea <samygeo2002@yahoo.com>
2020-10-24 12:59:52 +0300

    Add project files.

---
+++ MyPhoneNumbersTableAdapter.cs
@@ -0,0 +1,18 @@
+﻿
+using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.IO;
+using System.Linq;
+using System.Text;
+
+namespace Oanea_George_Lab5b.PhoneNumbersDataSetTableAdapters
+{
+    public partial class PhoneNumbersTableAdapter
+    {
+        public PhoneNumbersTableAdapter(String connectionString) { 
+            this.ClearBeforeFill = true; 
+            this.Connection = new System.Data.SqlClient.SqlConnection(connectionString); 
+        }
+    }
+}
